627597139816e189c49d4aceb5e5cb430718ea08
Pedro Acevedo <acevedoensisopedrogabriel@gmail.com>
2024-05-31 17:07:32 -0700

    Initial view implementation: pokemon list request, modeling and view design

--- PokedexV2/ContentView.swift
+++ PokedexV2/ContentView.swift
@@ -8,14 +8,133 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var entries: [PokedexEntry] = []
+    var columns: [GridItem] = [
+        GridItem(.adaptive(minimum: 180))
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(colors: [.blue, .white]),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            VStack {
+                ScrollView {
+                    Rectangle()
+                        .foregroundColor(.black.opacity(0))
+                        .frame(maxWidth: .infinity, minHeight: 60)
+                    LazyVGrid(columns: columns) {
+                        ForEach(entries) { i in
+                            ZStack {
+                                Rectangle()
+                                    .fill(
+                                        LinearGradient(
+                                            gradient: Gradient(colors: [i.color, i.color.opacity(0.4)]),
+                                            startPoint: .topLeading,
+                                            endPoint: .bottomTrailing
+                                        )
+                                    )
+                                    .frame(width: 180, height: 180)
+                                VStack {
+                                    AsyncImage(url: URL(string: i.image)) { phase in
+                                        switch phase {
+                                        case .empty:
+                                            ProgressView()
+                                                .frame(width: 90, height: 90)
+                                        case .success(let image):
+                                            image.resizable()
+                                                .frame(width: 90, height: 90)
+                                        case .failure:
+                                            Image(systemName: "questionmark")
+                                        @unknown default:
+                                            Image(systemName: "questionmark")
+                                        }
+                                    }
+                                    Text(i.name)
+                                    HStack {
+                                        ForEach(i.pokemonForms.first?.types ?? []) { type in
+                                            ZStack {
+                                                RoundedRectangle(cornerRadius: 25)
+                                                    .fill(type.color)
+                                                    .stroke(.white, lineWidth: 1)
+                                                    .frame(width: 80, height: 30)
+                                                Text(type.pokemonType.name)
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            .clipShape(RoundedRectangle(cornerRadius: 25))
+                        }
+                    }
+                    Rectangle()
+                        .foregroundColor(.black.opacity(0))
+                        .frame(maxWidth: .infinity, minHeight: 40)
+                }
+            }
+            VStack(spacing: 0) {
+                Rectangle()
+                    .foregroundColor(.red)
+                    .frame(maxWidth: .infinity, maxHeight: 50)
+                Rectangle()
+                    .foregroundColor(.black)
+                    .frame(maxWidth: .infinity, maxHeight: 8)
+                Spacer()
+                Rectangle()
+                    .foregroundColor(.black)
+                    .frame(maxWidth: .infinity, maxHeight: 8)
+                Rectangle()
+                    .foregroundColor(.red)
+                    .frame(maxWidth: .infinity, maxHeight: 30)
+            }
         }
-        .padding()
+        .task {
+            await loadData()
+        }
+        .ignoresSafeArea()
+    }
+    
+    func loadData() async {
+        let url = URL(string: "https://beta.pokeapi.co/graphql/v1beta")!
+        let query = """
+            query PokeAPIquery {
+              pokedexEntries: pokemon_v2_pokemonspecies(limit: 50, offset: 0, distinct_on: id) {
+                id
+                name
+                pokemonForms: pokemon_v2_pokemons {
+                  id
+                  name
+                  pokemonSpeciesId: pokemon_species_id
+                  isDefault: is_default
+                  types: pokemon_v2_pokemontypes {
+                    id
+                    pokemonType: pokemon_v2_type {
+                      name
+                    }
+                  }
+                  pokemonSprites: pokemon_v2_pokemonsprites {
+                    id
+                    sprites(path: "other")
+                  }
+                }
+              }
+            }
+        """
+        
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = try? JSONSerialization.data(withJSONObject: ["query": query, "variables": nil], options: [])
+        
+        do {
+            let response = try await URLSession.shared.data(for: request)
+            let decodedPokedex = try JSONDecoder().decode(PokedexData.self, from: response.0)
+            entries = decodedPokedex.data.pokedexEntries
+        } catch {
+            print("Error: \(error.localizedDescription)")
+        }
     }
 }
 
---
+++ PokedexV2/Models/Pokedex.swift
@@ -0,0 +1,152 @@
+//
+//  PokedexEntry.swift
+//  PokedexV2
+//
+//  Created by Pedro Acevedo on 31/05/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct PokedexData: Codable {
+    var data: Pokedex
+}
+
+struct Pokedex: Codable {
+    var pokedexEntries: [PokedexEntry]
+}
+
+struct PokedexEntry: Codable, Identifiable {
+    var id: Int
+    var name: String
+    var pokemonForms: [PokemonForm]
+    
+    var color: Color {
+        switch pokemonForms.first?.types.first?.pokemonType.name {
+        case "grass":
+            return Color(.grass)
+        case "fire":
+            return Color(.fire)
+        case "water":
+            return Color(.water)
+        case "bug":
+            return Color(.bug)
+        case "electric":
+            return Color(.electric)
+        case "rock":
+            return Color(.rock)
+        case "ground":
+            return Color(.ground)
+        case "dark":
+            return Color(.dark)
+        case "fighting":
+            return Color(.fighting)
+        case "steel":
+            return Color(.steel)
+        case "normal":
+            return Color(.normal)
+        case "ghost":
+            return Color(.ghost)
+        case "poison":
+            return Color(.poison)
+        case "dragon":
+            return Color(.dragon)
+        case "flying":
+            return Color(.flying)
+        case "psychic":
+            return Color(.psychic)
+        case "fairy":
+            return Color(.fairy)
+        case "ice":
+            return Color(.ice)
+        default:
+            return .white
+        }
+    }
+    
+    var image: String { pokemonForms.first?.pokemonSprites.first?.sprites.officialArtwork?.frontDefault ?? ""}
+}
+
+struct PokemonForm: Codable, Identifiable {
+    var id: Int
+    var name: String
+    var pokemonSpeciesId: Int
+    var isDefault: Bool
+    var types: [PokemonTypeData]
+    var pokemonSprites: [PokemonSprite]
+}
+
+struct PokemonTypeData: Codable, Identifiable {
+    var id: Int
+    var pokemonType: PokemonType
+    
+    var color: Color {
+        switch pokemonType.name {
+        case "grass":
+            return Color(.grass)
+        case "fire":
+            return Color(.fire)
+        case "water":
+            return Color(.water)
+        case "bug":
+            return Color(.bug)
+        case "electric":
+            return Color(.electric)
+        case "rock":
+            return Color(.rock)
+        case "ground":
+            return Color(.ground)
+        case "dark":
+            return Color(.dark)
+        case "fighting":
+            return Color(.fighting)
+        case "steel":
+            return Color(.steel)
+        case "normal":
+            return Color(.normal)
+        case "ghost":
+            return Color(.ghost)
+        case "poison":
+            return Color(.poison)
+        case "dragon":
+            return Color(.dragon)
+        case "flying":
+            return Color(.flying)
+        case "psychic":
+            return Color(.psychic)
+        case "fairy":
+            return Color(.fairy)
+        case "ice":
+            return Color(.ice)
+        default:
+            return .white
+        }
+    }
+}
+
+struct PokemonType: Codable {
+    var name: String
+}
+
+struct PokemonSprite: Codable, Identifiable {
+    var id: Int
+    var sprites: Sprite
+}
+
+struct Sprite: Codable {
+    var officialArtwork: Artwork?
+    
+    enum CodingKeys: String, CodingKey {
+        case officialArtwork = "official-artwork"
+    }
+}
+
+struct Artwork: Codable {
+    var frontDefault: String?
+    var frontShiny: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case frontDefault = "front_default"
+        case frontShiny = "front_shiny"
+    }
+}
